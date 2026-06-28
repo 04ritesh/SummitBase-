d15c044527d06072c224ba8a93c52059f13fa7a6
Ritesh Kumar <rockyhandsome1944@gmail.com>
2026-06-28 17:24:58 +0530

    initial spring boot project setup

---
+++ src/main/java/com/trek/summitBase/SummitBaseApplication.java
@@ -0,0 +1,13 @@
+package com.trek.summitBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummitBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummitBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trek/summitBase/SummitBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trek.summitBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummitBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
